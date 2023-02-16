4d5c91320fc4c1c696a0f8e6c43884f5c2551401
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-02-16 17:48:35 +0530

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterProductsPipe } from './shared/filter-products.pipe';
+import { HomeComponent } from './component/home/home.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
+    FilterProductsPipe,
+    HomeComponent,
+    SignupComponent,
+    CheckoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

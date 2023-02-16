9c7f0b5608d10d4f64b6080138553d5e1d4c87df
UJadhav01 <111181029+UJadhav01@users.noreply.github.com>
2023-02-16 17:50:18 +0530

    home.component.ts

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
